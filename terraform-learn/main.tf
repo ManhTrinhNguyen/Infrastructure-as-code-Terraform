85c0516807dd6337ca50c4cd2eef81d033e42c3d
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2025-04-03 14:29:04 -0700

    Add terraform learn directory

--- terraform-learn/main.tf
+++ terraform-learn/main.tf
@@ -4,13 +4,19 @@ provider "aws" {
   secret_key = ""
 }
 
+variable "vpc_cidr_block" {
+  description = "vpc cidr block"
+}
+variable "subnet_cidr_block" {
+  description = "subnet cidr block"
+}
 resource "aws_vpc" "development-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr_block
 }
 
 resource "aws_subnet" "dev-subnet-1" {
   vpc_id = aws_vpc.development-vpc.id
-  cidr_block = "10.0.10.0/24"
+  cidr_block = var.subnet_cidr_block
   availability_zone = "us-west-1"
 }
 
@@ -22,4 +28,12 @@ resource "aws_subnet" "dev-subnet-2" {
   vpc_id = data.aws_vpc.existing_vpc
   cidr_block = "172.31.48.0/20"
   availability_zone = "us-west-1"
+}
+ 
+output "dev-vpc-id" {
+  value = aws_vpc.development-vpc.id
+} 
+
+output "dev-subnet-id" {
+  value = aws_subnet.dev-subnet-1.id
 }
